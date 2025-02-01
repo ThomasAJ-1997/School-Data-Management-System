6df2cdc2404da408367233818fc5d7347c581a8d
Thomas Jones <Thomasajones@hotmail.co.uk>
2025-02-01 19:27:02 +0000

    contact commit;

--- index.php
+++ index.php
@@ -19,14 +19,14 @@
 <body class="home-section">
     <div class="home-section-black-fill"><br /> <br>
         <div class="home-section-container">
-            <nav>
+            <nav class="nav-glass">
                 <a href="#" class="logo">Student Compass</a>
 
                 <ul class="navbar">
-                    <li><a href="#" class="active">Home</a></li>
-                    <li><a href="#" class="active">About</a></li>
-                    <li><a href="#" class="active">Service</a></li>
-                    <li><a href="#" class="active">Contact</a></li>
+                    <li><a href="#home" class="active">Home</a></li>
+                    <li><a href="#about" class="active">About</a></li>
+                    <li><a href="#" class="active">Services</a></li>
+                    <li><a href="#contact" class="active">Contact</a></li>
                 </ul>
 
                 <div class="main">
@@ -37,44 +37,97 @@
                         <span></span>
                         <span></span>
                     </div>
-
-
-
-
                 </div>
             </nav>
             <div>
-                <section class="welcome-text d-flex justify-content-center align-items-center flex-column">
+
+                <section id="home" class="welcome-text d-flex justify-content-center align-items-center flex-column">
                     <img src="icon.png" alt="ICON Student Compass">
                     <h4> Welcome to Student Compass </h4>
                     <p>Student Data Management System designed for you in mind</p>
                 </section>
 
-                <section id="about" class="welcome-text d-flex justify-content-center align-items-center flex-column">
-
+                <section id="about" class="about-section">
+                    <div class="about-box">
+                        <h1 class="section-heading">About Us</h1>
+                        <p class="section-description">
+                            Student Compass collects, stores, and analyzes student data allowing educators to gain deeper insights into student performance, identify patterns, and make data-driven decisions to personalize learning experiences and support student success, all while automating many administrative tasks related to student information.
+                            <br> <br>
+                            For more information please feel free to contact us.
+                        </p>
+                        <div class="section-button">
+                            <a class="home-button" href="#">Contact Us</a>
+                        </div>
+                    </div>
                 </section>
 
-                <section id="contact" class="welcome-text d-flex justify-content-center align-items-center flex-column">
+                <section id="contact" class="contact-section">
+                    <div class="contact-box">
+                        <h2> Contact us </h2>
 
+
+                        <div class="row1">
+                            <div class="col">
+                                <div class="input-box">
+                                    <input type="text" name="" required="required">
+                                    <span class="contact-text">Fist Name</span>
+                                    <span class="line"></span>
+                                </div>
+                            </div>
+
+                            <div class="col">
+                                <div class="input-box">
+                                    <input type="text" name="" required="required">
+                                    <span class="contact-text">Last Name</span>
+                                    <span class="line"></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row1">
+                            <div class="col">
+                                <div class="input-box">
+                                    <input type="text" name="" required="required">
+                                    <span class="contact-text">Email</span>
+                                    <span class="line"></span>
+                                </div>
+                            </div>
+
+                            <div class="col">
+                                <div class="input-box">
+                                    <input type="text" name="" required="required">
+                                    <span class="contact-text">Mobile</span>
+                                    <span class="line"></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row1">
+                            <div class="col">
+                                <div class="input-box textarea">
+                                    <textarea required="" name="" id=""></textarea>
+                                    <span class="contact-text">Type your message here...</span>
+                                    <span class="line"></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row1">
+                            <div class="col">
+                                <input class="contact-button" type="submit" value="Send">
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </div>
         </div>
     </div>
+
 
     <!-- BOOTSTRAP -->
     <script src="js/script.js"></script>
     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-
-<script>
-    const hamMenu = document.querySelector(".hamburger");
-    const navMenuPopUp = document.querySelector(".navbar");
-
-    hamMenu.addEventListener("click", () => {
-        hamMenu.classList.toggle("active");
-        navMenuPopUp.classList.toggle("active");
-    });
-</script>
 
 </html>
